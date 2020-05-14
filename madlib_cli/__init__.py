67bd7dd88bb097a824718728158c0c7a1cede86c
corey-marchand <corey.marchand@me.com>
2020-05-14 08:38:44 -0700

    first commit

---
+++ madlib_cli/__init__.py
@@ -0,0 +1,21 @@
+
+def mad_lib_game():
+    Adjective_one = input("Enter a adjective: ")
+    Adjective_two = input("Enter another adjective: ")
+    Adjective_three = input("Enter another adjective: ")
+    Adjective_four = input("Enter another adjective: ")
+    Large_animal = input("Enter the name of a large animal: ")
+    Small_animal = input("Enter the name of a small animal: ")
+    girls_name = input("Enter a female name: ")
+    Adjective_five = input("Enter another adjective: ")
+    Adjective_six = input("Enter another adjective: ")
+    Plural_noun = input("Enter a plural noun: ")
+    Number = input("Enter a number between 1 and 50: ")
+    First_name = input("Enter a first name: ")
+    past_tense_verb = input("Enter a past tense verb: ")
+    
+    print(f"I the {Adjective_one} and {Adjective_two} {First_name} have {past_tense_verb}{First_name}'s {Adjective_three} sister and plan to steal her {Adjective_four} {Plural_noun}!")
+    print(f"What are a {Large_animal} and backpacking {Small_animal} to do? Before you can help {girls_name}, you'll have to collect the {Adjective_five} {Plural_noun} and {Adjective_six} {Plural_noun} that open up the {Number} worlds connected to A {First_name} Lair.")
+    print(f"There are {Number} {Plural_noun} and {Number} {Plural_noun} in the game, along with hundreds of other goodies for you to find.")
+
+mad_lib_game()
